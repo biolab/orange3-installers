5738b12c7f6f40111b7b4c659f4a978d55cba1ec
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2017-09-29 19:04:58 +0200

    appveyor: Allow use of prebuild conda-forge packages for release

--- ci/appveyor-conda-build.bat
+++ ci/appveyor-conda-build.bat
@@ -1,4 +1,5 @@
 @echo off
+setlocal EnableDelayedExpansion
 
 if "%PYTHON_VERSION%" == "" (
     echo PYTHON_VERSION must be defined >&2
@@ -10,25 +11,30 @@ if  "%PLATTAG%" == "" (
     exit /b 1
 )
 
-"%CONDA%" config --append channels conda-forge  || exit /b %ERRORLEVEL%
-"%CONDA%" install --yes conda-build=2.1.15      || exit /b %ERRORLEVEL%
+"%CONDA%" config --append channels conda-forge  || exit /b !ERRORLEVEL!
 
-"%CONDA%" build --python %PYTHON_VERSION% ./conda-recipe || exit /b %ERRROLEVEL%
+if "%BUILD_LOCAL%" == "" (
+    "%CONDA%" install --yes conda-build=2.1.15      || exit /b !ERRORLEVEL!
+    "%CONDA%" build --python %PYTHON_VERSION% ./conda-recipe || exit /b !ERRORLEVEL!
 
-rem Copy the build conda pkg to artifacts dir
-rem and the cache\conda-pkgs which is used later by build-conda-installer
-rem script
+    rem # Copy the build conda pkg to artifacts dir
+    rem # and the cache\conda-pkgs which is used later by build-conda-installer
+    rem # script
 
-mkdir ..\conda-pkgs        || exit /b %ERRROLEVEL%
-mkdir ..\cache             || exit /b %ERRROLEVEL%
-mkdir ..\cache\conda-pkgs  || exit /b %ERRROLEVEL%
-for /f %%s in ( '"%CONDA%" build --output --python %PYTHON_VERSION% conda-recipe' ) do (
-    copy /Y "%%s" ..\conda-pkgs\  || exit /b %ERRROLEVEL%
-    copy /Y "%%s" ..\cache\conda-pkgs\  || exit /b %ERRROLEVEL%
-)
+    mkdir ..\conda-pkgs        || exit /b !ERRORLEVEL!
+    mkdir ..\cache             || exit /b !ERRORLEVEL!
+    mkdir ..\cache\conda-pkgs  || exit /b !ERRORLEVEL!
 
-for /f %%s in ( '"%PYTHON%" setup.py --version' ) do (
-    set "VERSION=%%s"
+    for /f %%s in ( '"%CONDA%" build --output --python %PYTHON_VERSION% conda-recipe' ) do (
+        copy /Y "%%s" ..\conda-pkgs\  || exit /b !ERRORLEVEL!
+        copy /Y "%%s" ..\cache\conda-pkgs\  || exit /b !ERRORLEVEL!
+    )
+
+    for /f %%s in ( '"%PYTHON%" setup.py --version' ) do (
+        set "VERSION=%%s"
+    )
+) else (
+    set "VERSION=%BUILD_COMMIT%"
 )
 
 echo VERSION = %VERSION%
@@ -41,7 +47,7 @@ echo VERSION = %VERSION%
              scikit-learn=0.19.* ^
              bottleneck=1.2.* ^
              Orange3=%VERSION% ^
-    || exit /b %ERRROLEVEL%
+    || exit /b !ERRORLEVEL!
 
 "%CONDA%" list -n env --export --explicit --md5 > env-spec.txt
 
@@ -53,7 +59,7 @@ bash -e scripts/windows/build-conda-installer.sh ^
         --dist-dir dist ^
         --env-spec ./env-spec.txt ^
         --online no ^
-    || exit /b %ERRROLEVEL%
+    || exit /b !ERRORLEVEL!
 
 
 for %%s in ( dist/Orange3-*Miniconda*.exe ) do (
