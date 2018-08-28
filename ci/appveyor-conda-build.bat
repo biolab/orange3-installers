f5c8f1ccf416df3d1c7cf5268f2216a617ef5305
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2018-08-28 14:32:37 +0200

    Force PyQt5=5.6.* in the initial env to prevent/survive downgrade ...
    .. on future conda install invocations.
    
    see orange3 issue 3201

--- ci/appveyor-conda-build.bat
+++ ci/appveyor-conda-build.bat
@@ -47,6 +47,7 @@ echo VERSION = %VERSION%
              scipy=0.19.* ^
              scikit-learn=0.19.* ^
              bottleneck=1.2.* ^
+             pyqt=5.6.* ^
              Orange3=%VERSION% ^
     || exit /b !ERRORLEVEL!
 
