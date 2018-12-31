ff90bde7cd03c9d1949f555f2e6f1ee19dadbd5e
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2018-12-31 16:13:42 +0100

    appveyor: Fetch wheels beforehand

--- ci/appveyor-build.bat
+++ ci/appveyor-build.bat
@@ -30,6 +30,7 @@ if not "%BUILD_DEPS%" == "" (
 python -m pip list --format=freeze
 
 python -m pip wheel --no-deps -w ../wheels fastTSNE
+python -m pip wheel --no-deps -w ../wheels -f ../wheels -r "%ENVSPEC%"
 
 if not "%BUILD_LOCAL%" == "" (
     rem # https://bugs.python.org/issue29943
