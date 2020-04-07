32536d330ec69e1cb52cb3ac3db20c4490e4f14c
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2020-04-07 09:15:27 +0200

    build-conda-installer.sh: Check for PYTHON_VERSION

--- scripts/windows/build-conda-installer.sh
+++ scripts/windows/build-conda-installer.sh
@@ -382,6 +382,11 @@ if [[ ! "${VERSION}" ]]; then
     exit 1
 fi
 
+if [[ ! "${PYTHON_VERSION}" ]]; then
+    echo "Cannot determine pyhton version from the environment spec" >&2
+    exit 1;
+fi
+
 cp "${CACHEDIR:?}/miniconda/Miniconda3-${MINICONDA_VERSION}-Windows-${CONDAPLATTAG}.exe" \
    "${BASEDIR:?}/"
 
