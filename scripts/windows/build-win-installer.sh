b73db734cbf65fb02fc4e37924b06b2a76495eea
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2021-03-25 14:33:59 +0100

    build-win-installer.sh: Fix icon path

--- scripts/windows/build-win-installer.sh
+++ scripts/windows/build-win-installer.sh
@@ -339,7 +339,7 @@ EOF
              -DBASEDIR="${basedir}" \
              -DPYINSTALLER=${pyinstaller} \
              -DINSTALL_REGISTRY_KEY=OrangeCanvas \
-             -DINSTALLERICON=../scripts/windows/Orange.ico \
+             -DINSTALLERICON="$(win-path "${scriptdir}")/Orange.ico" \
              -DLICENSE_FILE="${BASEDIR}"/license.txt \
              -NOCD \
              -V4 -WX \
